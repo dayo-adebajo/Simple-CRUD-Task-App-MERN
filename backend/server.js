ac66a483b504a1b101541f25e678e256e94e9861
Adedayo Adebajo <adebajo.dayo@gmail.com>
2023-06-13 02:47:56 +0100

    completed backend of Simple CRUD Task App - MERN

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/connectDB");
+const mongoose = require("mongoose");
+const Task = require("./models/taskModel");
+const taskRoutes = require("./routes/taskRoute");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(taskRoutes);
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("Home page");
+});
+
+
+const PORT = process.env.PORT || 3000;
+
+const startServer = async () => {
+    try{
+        await connectDB();
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+};
+startServer();
